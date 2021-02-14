ee6a62e192617b0a832eddc17be003b208783dc8
armistrice <armistrice@gmail.com>
2021-02-15 01:39:38 +0300

    made less code

--- js/main.js
+++ js/main.js
@@ -3,44 +3,18 @@ const para1 = document.getElementById('text1');
 const para2 = document.getElementById('text2');
 const para3 = document.getElementById('text3');
 
-/* Я пытался все сделать так, чтобы не нужно было повторять один и тот же
-код  раза, но так, чтобы они работали независимо - не вышло. */
-
-let clicks1 = 0;
-
-function colorChange1() {
-  clicks1 += 1;
-  para1.style.color = colors[clicks1 - 1];
-  if (clicks1 > colors.length) {
-    clicks1 = 0;
-    para1.style.color = 'black';
-  }
+function colorChange(parameter) {
+  let clicks = 0;
+  return function () {
+    clicks += 1;
+    const paragraph = parameter;
+    paragraph.style.color = colors[clicks - 1];
+    if (clicks > colors.length - 1) {
+      clicks = 0;
+    }
+  };
 }
 
-para1.addEventListener('click', colorChange1);
-
-let clicks2 = 0;
-
-function colorChange2() {
-  clicks2 += 1;
-  para2.style.color = colors[clicks2 - 1];
-  if (clicks2 > colors.length) {
-    clicks2 = 0;
-    para2.style.color = 'black';
-  }
-}
-
-para2.addEventListener('click', colorChange2);
-
-let clicks3 = 0;
-
-function colorChange3() {
-  clicks3 += 1;
-  para3.style.color = colors[clicks3 - 1];
-  if (clicks3 > colors.length) {
-    clicks3 = 0;
-    para3.style.color = 'black';
-  }
-}
-
-para3.addEventListener('click', colorChange3);
+para1.addEventListener('click', colorChange(para1));
+para2.addEventListener('click', colorChange(para2));
+para3.addEventListener('click', colorChange(para3));
